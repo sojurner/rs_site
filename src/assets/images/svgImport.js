760021534e69f3f658d74f5dde8e2714d7a65d71
sojurner <pykim0591@gmail.com>
2019-03-14 20:12:02 -0600

    Add svg import export file

---
+++ src/assets/images/svgImport.js
@@ -0,0 +1,11 @@
+import Email from '../../assets/images/email.svg';
+import Github from '../../assets/images/github.svg';
+import Linkedin from '../../assets/images/linkedin.svg';
+import Phone from '../../assets/images/phone.svg';
+
+export default {
+  Email,
+  Github,
+  Linkedin,
+  Phone
+};
